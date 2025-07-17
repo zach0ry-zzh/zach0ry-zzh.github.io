15541244e0f86fd2f60bf2d0dccc006f9fde8081
zzh202408064212 <2794813078@qq.com>
2025-07-17 20:00:11 +0800

    Site updated: 2025-07-17 20:00:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/11/hello-world/","2025/07/05/pwn-study/","2025/07/11/HDCTF-2023-enc/","2025/07/14/HNCTF-2022-WEEK4-flower-plus/","2025/03/19/ida使用-动调/","2025/07/10/picoCTF2022/","2025/04/11/BUU-crackMe/"];function toRandomPost(){
+var posts=["2025/07/11/HDCTF-2023-enc/","2025/07/14/HNCTF-2022-WEEK4-flower-plus/","2025/03/19/ida使用-动调/","2025/07/10/picoCTF2022/","2025/07/16/NISACTF-2022-ezpie/","2025/07/16/控制命令/","2025/07/05/初识栈溢出/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
